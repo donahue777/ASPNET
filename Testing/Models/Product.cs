e73c40dbf6a145b3d6258da07bb118db3021dd8e
Thomas Donahue <donahue777@yahoo.com>
2024-08-22 14:32:22 -0400

    Create Product Complete

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Testing.Models
 {
@@ -10,5 +11,6 @@ namespace Testing.Models
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
